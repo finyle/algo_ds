3f4f5be3a615d5539b9b0bca78da7fd6a21a852f
finyle <linshihong96@gmail.com>
2024-06-30 15:30:47 +0800

    multask

---
+++ leetcode_multitask.go
@@ -0,0 +1,30 @@
+import "fmt"
+
+type FooBar struct {
+    n   intanA 
+    chanA   chan    bool
+    chanB   chan    bool
+}
+func NewFooBar(n int) *FooBar {
+    return &FooBar{n:n, chanA: make(chan bool, 1), chanB: make(chan bool)}
+}
+
+func (fb *FooBar) Foo(printFoo func()) {
+    for i := 0; i < fb.n; i++ {
+        fb.chanA <- true 
+        printFoo()
+        fb.chanB <- true 
+    }
+}
+
+func (fb *Foobar) Bar(printBar func()) {
+    for i := 0; i < fb.n; i ++ {
+        <- fb.chanB
+        printBar()
+        <- fb.ChanA 
+    }
+}
+
+func main() {
+    fmt.Println("hi")
+}
